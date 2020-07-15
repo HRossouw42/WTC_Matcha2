3084ae05ceb8759fd002cd3756a8b4f461f3f6b2
Harmun <harmun.rossouw@coil.com>
2020-07-15 16:32:24 +0200

    one way sort

--- frontend/src/search/usersList/Index.jsx
+++ frontend/src/search/usersList/Index.jsx
@@ -4,39 +4,39 @@ import { Link } from 'react-router-dom';
 import { accountService, alertService } from '@/_services';
 
 // https://www.smashingmagazine.com/2020/03/sortable-tables-react/
-const useSortableData = (items, config = null) => {
-  const [sortConfig, setSortConfig] = useState(config);
+// const useSortableData = (items, config = null) => {
+//   const [sortConfig, setSortConfig] = useState(config);
 
-  const sortedItems = useMemo(() => {
-    let sortableItems = [...items];
-    if (sortConfig !== null) {
-      sortableItems.sort((a, b) => {
-        if (a[sortConfig.key] < b[sortConfig.key]) {
-          return sortConfig.direction === 'ascending' ? -1 : 1;
-        }
-        if (a[sortConfig.key] > b[sortConfig.key]) {
-          return sortConfig.direction === 'ascending' ? 1 : -1;
-        }
-        return 0;
-      });
-    }
-    return sortableItems;
-  }, [items, sortConfig]);
+//   const sortedItems = useMemo(() => {
+//     let sortableItems = [...items];
+//     if (sortConfig !== null) {
+//       sortableItems.sort((a, b) => {
+//         if (a[sortConfig.key] < b[sortConfig.key]) {
+//           return sortConfig.direction === 'ascending' ? -1 : 1;
+//         }
+//         if (a[sortConfig.key] > b[sortConfig.key]) {
+//           return sortConfig.direction === 'ascending' ? 1 : -1;
+//         }
+//         return 0;
+//       });
+//     }
+//     return sortableItems;
+//   }, [items, sortConfig]);
 
-  const requestSort = (key) => {
-    let direction = 'ascending';
-    if (
-      sortConfig &&
-      sortConfig.key === key &&
-      sortConfig.direction === 'ascending'
-    ) {
-      direction = 'descending';
-    }
-    setSortConfig({ key, direction });
-  };
+//   const requestSort = (key) => {
+//     let direction = 'ascending';
+//     if (
+//       sortConfig &&
+//       sortConfig.key === key &&
+//       sortConfig.direction === 'ascending'
+//     ) {
+//       direction = 'descending';
+//     }
+//     setSortConfig({ key, direction });
+//   };
 
-  return { items: sortedItems, requestSort };
-};
+//   return { items: sortedItems, requestSort };
+// };
 
 function UsersList({ match }) {
   const { path } = match;
@@ -44,18 +44,54 @@ function UsersList({ match }) {
   const [users, setUsers] = useState(null);
 
   //sorting
-  // const [items, requestSort] = useSortableData(users);
+  // const [items, requestSort] = useSortableData(null);
+  const [sortedField, setSortedField] = useState(null);
+  const [sortDirection, setSortDirection] = useState(null);
 
   useEffect(() => {
     accountService
       .getAll()
-      .then((data) => setUsers(data))
+      .then((data) => {
+        let compiledData = data.map((obj) => {
+          let newObj = {};
+          newObj['id'] = obj.id;
+          newObj['firstName'] = obj.firstName;
+          newObj['lastName'] = obj.lastName;
+          newObj['location'] = obj.location;
+          newObj['fame'] = obj.fame;
+          newObj['age'] = obj.age;
+          newObj['smoking'] = obj.smoking;
+          newObj['drinking'] = obj.drinking;
+          newObj['religion'] = obj.religion;
+          newObj['pets'] = obj.pets;
+          newObj['children'] = obj.children;
+
+          return newObj;
+        });
+        setUsers(compiledData);
+      })
       .catch((error) => {
         alertService.error(error);
       });
   }, []);
   //numArray.sort((a, b) => a - b)
   //homes.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+
+  function requestSort(key) {
+    let sortedUsers = users;
+
+    sortedUsers.sort((a, b) => {
+      if (a[key] < b[key]) {
+        return -1;
+      }
+      if (a[key] > b[key]) {
+        return 1;
+      }
+      return 0;
+    });
+    setUsers(sortedUsers);
+    setSortedField(key);
+  }
 
   return (
     <div>
